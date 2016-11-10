f0e8bb973784a5dd859a2db449af66179b2d4383
Joseph Daniel Chapdelaine <orvist@gmail.com>
2016-11-10 11:39:27 -0600

    rendering html from client side

--- server/server.js
+++ server/server.js
@@ -6,8 +6,10 @@ const app = express()
 
 const PORT = process.env.PORT || 3000
 
+app.use(express.static('client'))
+
 app.get('/', (req, res) => {
-	res.send('Hello')
+	res.render('index')
 })
 
 app.listen(PORT, () => console.log(`port listening on: ${PORT}`))
