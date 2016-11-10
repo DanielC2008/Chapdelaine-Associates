98b8a3000ee0d3d8a05a2dda590af3bc96e4abaa
Joseph Daniel Chapdelaine <orvist@gmail.com>
2016-11-10 12:24:50 -0600

    added angular and angular route and basic controllers/partials

---
+++ client/app/main.js
@@ -0,0 +1,35 @@
+'use strict'
+
+angular
+	.module('Database', ['ngRoute'])
+	.config( $routeProvider =>
+		$routeProvider
+			.when('/', {
+				controller: 'Home',
+				templateUrl: 'partials/home.html'
+			})
+			.when('/newJob', {
+				controller: 'NewJob',
+				templateUrl: 'partials/newJob.html'
+			})
+			.when('/findJob', {
+				controller: 'FindJob',
+				templateUrl: 'partials/findJob.html'
+			})
+			.when('/getData', {
+				controller: 'GetData',
+				templateUrl: 'partials/getData.html'
+			})
+	)
+	.controller('Home', function($scope) {
+		$scope.title = "Home"
+	})
+	.controller('NewJob', function($scope) {
+		$scope.title = "NewJob"
+	})
+	.controller('FindJob', function($scope) {
+		$scope.title = "FindJob"
+	})
+	.controller('GetData', function($scope) {
+		$scope.title = "GetData"
+	})
--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.static('client'))
 
+app.set('view engine', 'pug')
+
 app.get('/', (req, res) => {
 	res.render('index')
 })
