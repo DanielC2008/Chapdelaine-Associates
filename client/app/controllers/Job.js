854d42cc8320f6b252e719d8097835575ae15e6c
Joseph Daniel Chapdelaine <orvist@gmail.com>
2016-12-30 08:02:50 -0600

    cleaned up Jobs controller

--- client/app/controllers/Job.js
+++ client/app/controllers/Job.js
@@ -3,14 +3,12 @@
   app.controller('Job', function($scope, $location, $http) {
     $scope.title = "Job"
 
-    let URL = $location.$$url
-    let sliceBegin = parseInt(URL.search(":")) + 1 
-    let jobNumber = URL.slice(sliceBegin)
+    let URL = $location.$$url 
+    let jobNumber = URL.slice(parseInt(URL.search(":")) + 1)
 
     $http.post('/getJob', {jobNumber})
       .success( job => {
         //everything job related gets set to the scope
-        // console.log('job', job);
         $scope.title = `Job Number: ${job.job.jobNumber}`
       })
       .error( data => {
