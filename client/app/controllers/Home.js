d027643eab5f91eda954e12a74581764b82e202b
Joseph Daniel Chapdelaine <orvist@gmail.com>
2016-12-31 12:48:28 -0600

    ng-include to add partial and controller to home page: home page will be broken out into 3 modules

--- client/app/controllers/Home.js
+++ client/app/controllers/Home.js
@@ -1,11 +1,11 @@
 'use strict'
 
 app.controller('Home', function($scope, $http, $rootScope) {
-    $http.get('/home')
-    .success( data => {
-      $scope.recentJobs = data
-    })
-    .error( () => {
-      console.log('error')
-    })
+    // $http.get('/home')
+    // .success( data => {
+    //   $scope.recentJobs = data
+    // })
+    // .error( () => {
+    //   console.log('error')
+    // })
   })
