8eaa6c5ff6a739673206745074800dd4518fdef6
Joseph Daniel Chapdelaine <orvist@gmail.com>
2017-01-15 13:01:41 -0600

    clicking a job in active or pending sends you to that job

--- client/app/controllers/ListJob.js
+++ client/app/controllers/ListJob.js
@@ -1,7 +1,10 @@
 'use strict'
 
-app.controller('ListJob', function($scope) {
+app.controller('ListJob', function($scope, $location) {
   let LJScope = this
 
+  LJScope.getJob = jobNumber => {
+    $location.path(`/jobs/:${jobNumber}`)
+  }
 
 })
