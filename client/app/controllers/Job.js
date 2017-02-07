3a062ff160896241fadddab946c8e59919b3d591
Joseph Daniel Chapdelaine <orvist@gmail.com>
2017-02-07 11:03:55 -0600

    moved JobMain JS out of Job controller

--- client/app/controllers/Job.js
+++ client/app/controllers/Job.js
@@ -1,13 +1,10 @@
 "use strict"
 
-  app.controller('Job', function($scope, $location, JobFactory, $route) {
-    //========brings back info and makes all fields input until save
+  app.controller('Job', function($scope, $location, JobFactory) {
     let URL = $location.$$url
-    $scope.editAll = URL.match('editAll') ? true : false
     $scope.jobNumber = $scope.editAll === true ? URL.slice(parseInt(URL.search(":") + 1), URL.lastIndexOf('/')) : URL.slice(parseInt(URL.search(":") + 1)) 
     $scope.showTab = 'JobMain'
-    $scope.editOptions = {}
-    let editCanceled = {}
+
     //load data from database
     JobFactory.getJobFromDatabase($scope.jobNumber)
       .then( ({data}) => {
@@ -23,66 +20,5 @@
       })
       //can post status with .status and .statusText
       .catch( () => alert('Wooops. There doesn\'t seem to be anything here!'))
-
-    //edit data submited by user    
-    $scope.editDatabase = (table, id, key, value) => {
-      removeEditOptions() 
-      //make sure user wants to make these changes
-      let obj = {}
-      //transform key to sql table name
-      obj[key.toLowerCase().replace(' ', '_')] = value
-      JobFactory.editColumn({table, id, obj})
-        .then( ({data: {msg}}) => alert(msg))
-        .catch( ({data: {msg}}) => alert(msg))
-    }
-
-    //set edit options on obj for easy comparison and edit canceled obj so data isn't lost 
-    $scope.setEditOptions = (editType, tableName, tableIndex, index, key, value) => {
-      $scope.editOptions.editType = editType
-      $scope.editOptions.tableName = tableName
-      $scope.editOptions.tableIndex = tableIndex
-      $scope.editOptions.inputIndex = index
-      editCanceled.key = key
-      editCanceled.value = value
-    }
-
-    //focuses specific editOne
-    $scope.changeFocus = (tableName, tableIndex, index) => {
-      if (index == $scope.inputIndex && tableIndex == $scope.tIndex && tableName == $scope.table) {
-        return true
-      }
-    }
-    
-    //if user cancels edit, reset ng-model
-    $scope.revertEditChanges = obj => {
-      removeEditOptions()    
-      obj[editCanceled.key] = editCanceled.value
-    }
-    //lodash for deep equals
-    $scope.compareObj = currDiv => _.isEqual($scope.editOptions, currDiv)
-    //set editOptions back to null
-    const removeEditOptions = () => {
-      for(let key in $scope.editOptions) {
-          $scope.editOptions[key] = null
-      } 
-    }
-
-
-    // $scope.editOrSave = () => {
-    //   $scope.edit ? $scope.edit = false: $scope.edit = true
-    //   //save to database//////////////////////////////////////////////
-    // }
-
-    $scope.removeFromJob = (table, objToRemove, job_number) => {
-      let dataObj = {
-        table,
-        objToRemove,
-        job_number
-      }
-      JobFactory.removeFromJob(dataObj)
-        .then( ({data}) => $route.reload())
-        .catch( ({data}) => console.log(data))
-    }
-
 
   })
--- client/app/controllers/JobMain.js
+++ client/app/controllers/JobMain.js
@@ -1,8 +1,57 @@
 'use strict'
 
-app.controller('JobMain', function($scope, $http) {
+app.controller('JobMain', function($scope, $location, JobFactory, $route, $http) {
   let JMScope = this
 
+
+/////////////FOR EDITING A SINGLE COLUMN///////////////
+  JMScope.editOptions = {}
+  let editCanceled = {}
+  //edit data submited by user    
+  JMScope.editDatabase = (table, id, key, value) => {
+    removeEditOptions() 
+    //make sure user wants to make these changes
+    let obj = {}
+    //transform key to sql table name
+    obj[key.toLowerCase().replace(' ', '_')] = value
+    JobFactory.editColumn({table, id, obj})
+      .then( ({data: {msg}}) => alert(msg))
+      .catch( ({data: {msg}}) => alert(msg))
+  }
+
+  //set edit options on obj for easy comparison and edit canceled obj so data isn't lost 
+  JMScope.setEditOptions = (editType, tableName, tableIndex, index, key, value) => {
+    JMScope.editOptions.editType = editType
+    JMScope.editOptions.tableName = tableName
+    JMScope.editOptions.tableIndex = tableIndex
+    JMScope.editOptions.inputIndex = index
+    editCanceled.key = key
+    editCanceled.value = value
+  }
+
+  //focuses specific editOne
+  JMScope.changeFocus = (tableName, tableIndex, index) => {
+    if (index == $scope.inputIndex && tableIndex == $scope.tIndex && tableName == $scope.table) {
+      return true
+    }
+  }
+  
+  //if user cancels edit, reset ng-model
+  JMScope.revertEditChanges = obj => {
+    removeEditOptions()    
+    obj[editCanceled.key] = editCanceled.value
+  }
+  //lodash for deep equals
+  JMScope.compareObj = currDiv => _.isEqual(JMScope.editOptions, currDiv)
+  //set editOptions back to null
+  const removeEditOptions = () => {
+    for(let key in JMScope.editOptions) {
+        JMScope.editOptions[key] = null
+    } 
+  }
+
+
+/////////////ADD OR REMOVE FROM JOB///////////////
   JMScope.addClientBySearch = () => {
     $http
       .get('api/getClientNames')//should pass in user_id here
@@ -16,6 +65,16 @@ app.controller('JobMain', function($scope, $http) {
     
   }
 
+  JMScope.removeFromJob = (table, objToRemove, job_number) => {
+    let dataObj = {
+      table,
+      objToRemove,
+      job_number
+    }
+    JobFactory.removeFromJob(dataObj)
+      .then( ({data}) => $route.reload())
+      .catch( ({data}) => console.log(data))
+  }
 
 
 })
