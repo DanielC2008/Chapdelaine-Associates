d97e2fa146507a7c30c3498c3a516dadfd8c6cdd
Joseph Daniel Chapdelaine <orvist@gmail.com>
2017-02-17 14:48:52 -0600

    AddNew is using TableAndColumn

--- client/app/controllers/AddNew.js
+++ client/app/controllers/AddNew.js
@@ -1,6 +1,6 @@
 'use strict'
 
-app.controller('AddNew', function($scope, $mdDialog, table, job_id, clientArray, JobFactory) {
+app.controller('AddNew', function($scope, $mdDialog, table, job_id, clientArray, JobFactory, TableAndColumnFactory) {
   let NEW = this
 
   switch(table) {
@@ -21,56 +21,7 @@ app.controller('AddNew', function($scope, $mdDialog, table, job_id, clientArray,
     NEW.ClientNames = clientArray
   }
 
-
-  NEW.Display = {
-    Clients: {
-      'First Name': '', 
-      'Middle Name': '', 
-      'Last Name': '', 
-      'Email': '', 
-      'Business Phone': '', 
-      'Mobile Phone': '', 
-      'Home Phone': '', 
-      'Fax Number': '', 
-      'Address': '', 
-      'City': '', 
-      'State': '', 
-      'Zip Code': '', 
-      'County': '', 
-      'Notes': '', 
-    }, 
-    Representatives: {
-      'First Name': '', 
-      'Middle Name': '', 
-      'Last Name': '', 
-      'Email': '', 
-      'Business Phone': '', 
-      'Mobile Phone': '', 
-      'Home Phone': '', 
-      'Fax Number': '', 
-      'Address': '', 
-      'City': '', 
-      'State': '', 
-      'Zip Code': '', 
-      'County': '', 
-      'Notes': '', 
-    }, 
-    Properties: {
-      'Address': '',
-      'City': '',
-      'State': '',
-      'Zip Code': '',
-      'County': '',
-      'Map': '',
-      'Parcel Number': '',
-      'Plat Book': '',
-      'Plat Page': '',
-      'Deed Book': '',
-      'Deed Page': '',
-      'Sub Division': '',
-      'Notes': ''
-    }
-  }
+  NEW.Display = TableAndColumnFactory
 
   NEW.send = ()  => {
     let objToAdd = JobFactory.matchDatabaseKeys(_.cloneDeep(NEW.Display[`${NEW.table}`]))
