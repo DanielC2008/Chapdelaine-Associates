d59921a514942bd6a477709e7977e162b92dc1e9
Joseph Daniel Chapdelaine <orvist@gmail.com>
2017-02-21 19:12:42 -0600

    refactored types of work api so it could be used by the more generic function used by everything except Jobs

--- server/DBHelper.js
+++ server/DBHelper.js
@@ -10,19 +10,19 @@ module.exports = {
         tableObj.tableName = 'Clients'
         tableObj.connectTable = 'Jobs_Clients'
         tableObj.returningId = 'client_id'
-        tableObj.findJobId = 'client_id'
+        tableObj.findJobId = 'job_id'
         break;
       case 'Representatives':
         tableObj.tableName = 'Representatives'
         tableObj.connectTable = 'Clients_Representatives'
         tableObj.returningId = 'representative_id'
-        tableObj.findJobId = 'representative_id'
+        tableObj.findJobId = 'job_id'
         break;
       case 'Properties':
         tableObj.tableName = 'Properties'
         tableObj.connectTable = 'Jobs_Properties'
         tableObj.returningId = 'property_id'
-        tableObj.findJobId = 'property_id'
+        tableObj.findJobId = 'job_id'
         break;
       case 'Types Of Work':
         tableObj.tableName = 'Types_Of_Work'
--- server/routes/findJob.js
+++ server/routes/findJob.js
@@ -14,43 +14,20 @@ router.post('/api/findJob', ({body}, res) => {
     let objToFind = param.objToFind
 
     if (param.table != 'Jobs') {
-      var {tableName, connectTable, returningId} = DBHelper.getTableInfo(param.table)
-    }else {
-      var tableName = param.table
-    }
-    
-    if (tableName == 'Clients' || tableName == 'Representatives' || tableName == 'Properties'){
+      let {tableName, connectTable, returningId, findJobId} = DBHelper.getTableInfo(param.table)
       knex(`${tableName}`)
         .select('Jobs.job_number')
         .join(`${connectTable}`, `${connectTable}.${returningId}`, `${tableName}.${returningId}`)
-        .join('Jobs', 'Jobs.job_id', `${connectTable}.job_id`)
+        .join('Jobs', `Jobs.${findJobId}`, `${connectTable}.${findJobId}`)
         .where(objToFind)
         .then( data => console.log('data', data))
-    } else if (tableName  == 'Jobs') {
-        knex(`${tableName}`)
-          .select('Jobs.job_number')
-          .where(objToFind)
-          .then( data => console.log('data', data)) 
-    } else if (tableName == 'Types_Of_Work') {
-        knex(`${tableName}`)
-          //get type of work id
-          .select('Types_Of_Work.type_of_work_id')
-          .where(objToFind)
-          .then( data => {
-            let type_of_work_id = data[0]
-            knex('Types_Invoices')
-              .select('Jobs.job_number')
-              .join('Jobs', 'Jobs.invoice_id', 'Types_Invoices.invoice_id')
-              .where(type_of_work_id)
-              .then( data => console.log('data', data))
-          })
-
+    } else {
+      knex(`${param.table}`)
+        .select('Jobs.job_number')
+        .where(objToFind)
+        .then( data => console.log('data', data)) 
     }
-  
   })
-  // returns a large obj of the thing requested and the job id
-
-  // for multiple params i need to promise.all
   // after the results come back i need to find matches that are stored in a param queries
   // send those in two groups //found in all params //found in < all params
 
