b9195e44c2dcd52e8909f94533ad90e710ac5cdb
Joseph Daniel Chapdelaine <orvist@gmail.com>
2017-03-28 10:31:51 -0500

    active and pending job scroll on home page retrieves actual data and goes to that job number

--- client/app/controllers/ListJob.js
+++ client/app/controllers/ListJob.js
@@ -3,6 +3,6 @@
 app.controller('ListJob', function($scope, JobFactory) {
   let LJScope = this
 
-  LJScope.getJob = jobNumber => JobFactory.goToJobPage(jobNumber)
+  LJScope.goToJobPage = jobNumber => JobFactory.goToJobPage(jobNumber)
 
 })
