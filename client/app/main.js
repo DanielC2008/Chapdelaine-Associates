827ee44fae130c0894680cc098ed4ea6ecfcde9c
Joseph Daniel Chapdelaine <orvist@gmail.com>
2017-03-28 12:50:14 -0500

    route provider and server route gaurd requires a login

--- client/app/main.js
+++ client/app/main.js
@@ -2,28 +2,24 @@
 
 
 const app = angular.module('Database', ['ngRoute', 'focus-if', 'ngMaterial', 'ngMessages', 'angularFileUpload'])
-  .run(function($rootScope, JobFactory) {
-    JobFactory.getUserName()
-      .then(({data}) => $rootScope.$user = data.user_name)
-  })
-	.config( $routeProvider =>
-		$routeProvider
+  .config( $routeProvider =>
+    $routeProvider
       .when('/login', {
         controller: 'Login-Register',
         templateUrl: 'partials/login-register.html'
       })
-			.when('/logout', {
-				controller: 'Logout',
+      .when('/logout', {
+        controller: 'Logout',
         templateUrl: 'partials/logout.html'
-			})
-			.when('/home', {
-				controller: 'Home',
-				templateUrl: 'partials/home.html'
-			})
-			.when('/newJob', {
-				controller: 'NewJob',
-				templateUrl: 'partials/newJob.html'
-			})
+      })
+      .when('/home', {
+        controller: 'Home',
+        templateUrl: 'partials/home.html'
+      })
+      .when('/newJob', {
+        controller: 'NewJob',
+        templateUrl: 'partials/newJob.html'
+      })
       .when('/getData', {
         controller: 'GetData',
         templateUrl: 'partials/getData.html'
@@ -36,8 +32,22 @@ const app = angular.module('Database', ['ngRoute', 'focus-if', 'ngMaterial', 'ng
         controller: 'Job',
         templateUrl: 'partials/job.html'
       })
-			.otherwise('/login')
-	)
-	
-	
+      .otherwise('/login')
+  )
+  .run( function($rootScope, $location, JobFactory) {
+    JobFactory.getUserName()
+      .then(({data}) => $rootScope.$user = data.user_name)
+    // register listener to watch route changes
+    $rootScope.$on( "$routeChangeStart", function(event, next, current) {
+      if ( $rootScope.$user == null ) {
+        if ( next.templateUrl == "partials/login-register.html" ) { //--------- already going to #login, no redirect needed
+        } else {  // -----------------------------------------------------------not going to login-register, redirect now
+          $location.path( "/login" )
+          alert('Please Log In.')
+        }
+      }         
+    })
+  })
+  
+  
 
