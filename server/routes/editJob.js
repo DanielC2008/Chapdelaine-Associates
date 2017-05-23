34904ea4cc14e2720021b2b3e80045080321734e
Joseph Daniel Chapdelaine <orvist@gmail.com>
2017-05-23 11:44:27 -0500

    removed unused code from edit job

--- server/routes/editJob.js
+++ server/routes/editJob.js
@@ -4,7 +4,6 @@ const { Router } = require('express')
 const config = require('../../database/knexfile.js').development
 const knex = require('knex')(config)
 const router = Router()
-const DBHelper = require('../DBHelper')
 
 router.post('/api/editColumn', ({body: {table, id, obj}}, res) => {
   knex(`${table}`)
@@ -15,66 +14,6 @@ router.post('/api/editColumn', ({body: {table, id, obj}}, res) => {
       console.log(err)
       res.send({msg: 'Something went wrong! Please try again.'})
     })
-})
-
-router.post('/api/removeFromJob', ({body: {table, objToRemove, job_id}}, res) => {
-  let {connectTable} = DBHelper.getTableInfo(table)
-
-  //-----------------------------------------------------------might end up sending job_id with original obj
-  knex('Jobs')
-    .where(job_id)
-    //then remove from the connecting table using both id's
-    .then( data => {
-      objToRemove.job_id = data[0].job_id
-      knex(`${connectTable}`)
-        .del()
-        .where(objToRemove)
-        .then( () => {
-          res.send({msg: 'Removed from Job!'})
-        }).catch( err => console.log(err))
-    }).catch( err => console.log(err))
-})
-
-router.post('/api/addToJob', ({body: {table, objToAdd, job_id}}, res) => {
-  let {connectTable} = DBHelper.getTableInfo(table)
-  knex('Jobs')
-    .where(job_id)
-    .then( data => {
-      objToAdd.job_id = data[0].job_id
-      knex(`${connectTable}`)
-        .insert(objToAdd)
-        .then( () => {
-          res.send({msg: 'Successfully added to Job!'})
-        }).catch( err => console.log(err))
-    }).catch( err => console.log(err))
-})
-
-
-router.post('/api/addNewToJob', ({body: {table, objToAdd, clientId, job_id}}, res) => {
-  let {tableName, returningId, connectTable} = DBHelper.getTableInfo(table)
-  let connectTableObj = {}
-  // find job number
-  knex('Jobs')
-    .where(job_id)
-    .then( data => {
-      connectTableObj.job_id = data[0].job_id
-      //make client
-      knex(`${tableName}`)
-        .returning(`${returningId}`)
-        .insert(objToAdd)
-        .then( data => {
-          connectTableObj[returningId] = data[0]
-          if (clientId) {
-            connectTableObj.client_id = clientId
-          }
-          //set ids on connecting table
-          knex(`${connectTable}`)
-          .insert(connectTableObj)
-          .then( () => {
-            res.send()
-          }).catch( err => console.log(err))
-        }).catch( err => console.log(err))
-    }).catch( err => console.log(err))
 })
 
 router.post('/api/setNewTab', ({body:{jobNumber, showTab}, session}, res) => {
