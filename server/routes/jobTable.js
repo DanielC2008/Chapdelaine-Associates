610c5a377f0c2ba18afbff99b9d1e0771b0bef9b
Joseph Daniel Chapdelaine <orvist@gmail.com>
2017-07-12 07:59:49 -0500

    no longer creating estimates and invoices before job is created

--- server/routes/jobTable.js
+++ server/routes/jobTable.js
@@ -7,19 +7,10 @@ const router = Router()
 const {validJobType} = require('../validation/validJob')
 
 router.post('/api/createNewJob', ({body}, res) => {
-  let newJob = body
-  //create invoice and estimate
-  Promise.all([
-    knex.raw('INSERT INTO Invoices OUTPUT Inserted.invoice_id DEFAULT VALUES')
-      .then(data => newJob.invoice_id = data[0].invoice_id),
-    knex.raw('INSERT INTO Estimates OUTPUT Inserted.estimate_id DEFAULT VALUES')
-      .then(data => newJob.estimate_id = data[0].estimate_id)
-  ]).then( () => {
-    knex('Jobs')
-      .insert(newJob)
-      .then( () => res.send({msg:'Success'}))
-      .catch( err => err.number === 2601 ? res.send({msg: "That number is in use. Please choose another."}) : console.log(err))
-  })
+  knex('Jobs')
+  .insert(body)
+  .then( () => res.send({msg:'Success'}))
+  .catch( err => err.number === 2601 ? res.send({msg: "That number is in use. Please choose another."}) : console.log(err))
 })
 
 router.post('/api/updateJobStatus', ({body: {jobObj, currJobNum}}, res) => {
