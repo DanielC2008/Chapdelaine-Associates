44604bf432b245eb8764a74d60a48aacf9029411
Joseph Daniel Chapdelaine <orvist@gmail.com>
2017-07-17 15:00:52 -0500

    fixed bug when checking type of job number in job status

--- client/app/controllers/JobStatus.js
+++ client/app/controllers/JobStatus.js
@@ -83,8 +83,10 @@ app.controller('JobStatus', function($scope, JobFactory, DBFactory, ToastFactory
     statusPath(status)
   }
 
-  $scope.numberSet = job_number => { //for RecommendedNumber
-    if (typeof job_number === 'number' && job_number > 0) {
+  $scope.numberSet = number => { //for RecommendedNumber
+    let job_number = Number(number)
+    console.log('job_number', job_number, typeof job_number)
+    if (job_number != NaN && job_number > 0) {
       $scope.Job.job_number = job_number
       submitJobStatus()
     } else {
