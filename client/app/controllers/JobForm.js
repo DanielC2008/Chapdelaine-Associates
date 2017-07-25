75ccc9a799290431a551098de77754af32b1f0aa
Joseph Daniel Chapdelaine <orvist@gmail.com>
2017-07-25 13:53:57 -0500

    client, clientContact, owner, and ownerContacts are all using the same form, validation and api

--- client/app/controllers/JobForm.js
+++ client/app/controllers/JobForm.js
@@ -11,9 +11,9 @@ app.controller('JobForm', function($scope, ToastFactory, job, PropertyFactory, C
     roads: {},
     client: {},
     clientType: {},
-    clientRep: {},
+    clientContact: {},
     owner: {},
-    ownerRep: {},
+    ownerContact: {},
     ids: {}
   }
   $scope.job = job ? job : defaultJob
@@ -22,6 +22,7 @@ app.controller('JobForm', function($scope, ToastFactory, job, PropertyFactory, C
   $scope.propertySet = Object.keys($scope.job.property).length === 0 ? false : true
   $scope.clientSet = Object.keys($scope.job.client).length === 0 ? false : true
   $scope.clientTypeSet = Object.keys($scope.job.clientType).length === 0 ? false : true
+  $scope.clientContactSet = Object.keys($scope.job.clientContact).length === 0 ? false : true
 
   $scope.showCause = cause => $scope.displayCause = cause 
 
@@ -50,8 +51,8 @@ app.controller('JobForm', function($scope, ToastFactory, job, PropertyFactory, C
       $scope.job.property = dbPackage.dbObj
     }).catch( err => err.msg ? ToastFactory.toastReject(err.msg) : console.log('err', err))
   }
-
-  $scope.addClient= () => { 
+/////////////////////////////////////////CLIENT/////////////////////////////////////////
+  $scope.addClient = () => { 
       //force user to search for client first
       CustomerFactory.searchForCustomers().then( customer_id => {
         if (customer_id) {
@@ -84,5 +85,105 @@ app.controller('JobForm', function($scope, ToastFactory, job, PropertyFactory, C
     }).catch( err => err.msg ? ToastFactory.toastReject(err.msg) : console.log('err', err))
   }
 
+/////////////////////////////////////////CLIENT CONTACT/////////////////////////////////////////
+  $scope.addClientContact = () => { 
+      //force user to search for contact first
+      CustomerFactory.searchForCustomers().then( customer_id => {
+        if (customer_id) {
+          //if exist bring back full contact in case they want to make changes
+          CustomerFactory.getFullCustomerById({customer_id}).then( ({data}) => {
+            //edit, validate, and on return set obj, clientContactSet, and id
+            CustomerFactory.editCustomer(data, data.customer_id).then( ({dbPackage, msg}) => {
+              ToastFactory.toastSuccess(msg)
+              $scope.job.clientContact = dbPackage.dbObj
+              $scope.clientContactSet = true
+              $scope.job.ids.clientContact_id = dbPackage.customer_id.customer_id
+            }).catch( err => err.msg ? ToastFactory.toastReject(err.msg) : console.log('err', err))
+          }).catch( err => err.msg ? ToastFactory.toastReject(err.msg) : console.log('err', err))
+        } else {
+          CustomerFactory.addCustomer().then( ({dbPackage, msg}) => {
+            ToastFactory.toastSuccess(msg)
+            $scope.job.clientContact = dbPackage.dbObj
+            $scope.clientContactSet = true
+          }).catch( err => err.msg ? ToastFactory.toastReject(err.msg) : console.log('err', err))
+        }
+      }).catch( err => err.msg ? ToastFactory.toastReject(err.msg) : console.log('err', err))
+  }
+
+  $scope.editClientContact = () => {
+    //send id for name check if id exists else we are editing a validated clientContact not in DB
+    let customer_id = $scope.job.ids.clientContact_id ? $scope.job.ids.clientContact_id : null 
+    CustomerFactory.editCustomer($scope.job.clientContact, customer_id).then( ({dbPackage, msg}) => {
+      ToastFactory.toastSuccess(msg)
+      $scope.job.clientContact = dbPackage.dbObj
+    }).catch( err => err.msg ? ToastFactory.toastReject(err.msg) : console.log('err', err))
+  }
+
+/////////////////////////////////////////OWNER/////////////////////////////////////////
+  $scope.addOwner = () => { 
+      //force user to search for owner first
+      CustomerFactory.searchForCustomers().then( customer_id => {
+        if (customer_id) {
+          //if exist bring back full owner in case they want to make changes
+          CustomerFactory.getFullCustomerById({customer_id}).then( ({data}) => {
+            //edit, validate, and on return set obj, ownerSet, and id
+            CustomerFactory.editCustomer(data, data.customer_id).then( ({dbPackage, msg}) => {
+              ToastFactory.toastSuccess(msg)
+              $scope.job.owner = dbPackage.dbObj
+              $scope.ownerSet = true
+              $scope.job.ids.owner_id = dbPackage.customer_id.customer_id
+            }).catch( err => err.msg ? ToastFactory.toastReject(err.msg) : console.log('err', err))
+          }).catch( err => err.msg ? ToastFactory.toastReject(err.msg) : console.log('err', err))
+        } else {
+          CustomerFactory.addCustomer().then( ({dbPackage, msg}) => {
+            ToastFactory.toastSuccess(msg)
+            $scope.job.owner = dbPackage.dbObj
+            $scope.ownerSet = true
+          }).catch( err => err.msg ? ToastFactory.toastReject(err.msg) : console.log('err', err))
+        }
+      }).catch( err => err.msg ? ToastFactory.toastReject(err.msg) : console.log('err', err))
+  }
+
+  $scope.editOwner = () => {
+    //send id for name check if id exists else we are editing a validated owner not in DB
+    let customer_id = $scope.job.ids.owner_id ? $scope.job.ids.owner_id : null 
+    CustomerFactory.editCustomer($scope.job.owner, customer_id).then( ({dbPackage, msg}) => {
+      ToastFactory.toastSuccess(msg)
+      $scope.job.owner = dbPackage.dbObj
+    }).catch( err => err.msg ? ToastFactory.toastReject(err.msg) : console.log('err', err))
+  }
+/////////////////////////////////////////OWNER CONTACT/////////////////////////////////////////
+  $scope.addOwnerContact = () => { 
+      //force user to search for contact first
+      CustomerFactory.searchForCustomers().then( customer_id => {
+        if (customer_id) {
+          //if exist bring back full contact in case they want to make changes
+          CustomerFactory.getFullCustomerById({customer_id}).then( ({data}) => {
+            //edit, validate, and on return set obj, ownerContactSet, and id
+            CustomerFactory.editCustomer(data, data.customer_id).then( ({dbPackage, msg}) => {
+              ToastFactory.toastSuccess(msg)
+              $scope.job.ownerContact = dbPackage.dbObj
+              $scope.ownerContactSet = true
+              $scope.job.ids.ownerContact_id = dbPackage.customer_id.customer_id
+            }).catch( err => err.msg ? ToastFactory.toastReject(err.msg) : console.log('err', err))
+          }).catch( err => err.msg ? ToastFactory.toastReject(err.msg) : console.log('err', err))
+        } else {
+          CustomerFactory.addCustomer().then( ({dbPackage, msg}) => {
+            ToastFactory.toastSuccess(msg)
+            $scope.job.ownerContact = dbPackage.dbObj
+            $scope.ownerContactSet = true
+          }).catch( err => err.msg ? ToastFactory.toastReject(err.msg) : console.log('err', err))
+        }
+      }).catch( err => err.msg ? ToastFactory.toastReject(err.msg) : console.log('err', err))
+  }
+
+  $scope.editOwnerContact = () => {
+    //send id for name check if id exists else we are editing a validated ownerContact not in DB
+    let customer_id = $scope.job.ids.ownerContact_id ? $scope.job.ids.ownerContact_id : null 
+    CustomerFactory.editCustomer($scope.job.ownerContact, customer_id).then( ({dbPackage, msg}) => {
+      ToastFactory.toastSuccess(msg)
+      $scope.job.ownerContact = dbPackage.dbObj
+    }).catch( err => err.msg ? ToastFactory.toastReject(err.msg) : console.log('err', err))
+  }
 
 })
