72d243c0a7bfa85d937647ff30898622a3c77846
Joseph Daniel Chapdelaine <orvist@gmail.com>
2017-08-08 17:01:54 -0500

    removing any code having to do with Clients table, remove clients table and clients_specs_per_job

--- client/app/controllers/UpdateStatus.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-app.controller('UpdateStatus', function($scope, job_info) { 
-  let US = this
-  $scope.tableForDB ='Jobs' //---------------for getMaxNumber on RN
-  $scope.job_info = job_info  //---setting current status
-
-})
