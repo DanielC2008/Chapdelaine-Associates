3a246142932328a79252aa37a5d7b5acf9ff9ca7
Joseph Daniel Chapdelaine <orvist@gmail.com>
2017-08-23 16:41:56 -0500

    removed unused code from SearchFactory

--- client/app/factories/SearchFactory.js
+++ client/app/factories/SearchFactory.js
@@ -24,29 +24,5 @@ app.factory('SearchFactory', function($mdDialog) {
     })  
   }
 
-  factory.chooseOne = (table, options) => {
-    let locals = { optionsArr : createArrForChooseOne(table, options) }
-    return new Promise ((resolve, reject) => {
-      $mdDialog.show({
-        locals,
-        controller: 'ChooseOne as CO',
-        templateUrl: '/partials/chooseOne.html',
-        parent: angular.element(document.body),
-        clickOutsideToClose:false
-      })
-      .then( id => resolve(id))
-      .catch(err => console.log(err))
-    })
-  }
-
-  const createArrForChooseOne = (table, options) => { //right now only works for clients and reps
-    return options.map( opt => {
-      return {
-        id: (table === 'Clients') ? opt.client_id : opt.representative_id,
-        name : `${opt.first_name} ${opt.last_name}`
-      }  
-    })
-  }
-
   return factory
 })
