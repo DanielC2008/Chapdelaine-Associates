0a8756956fafde78d8b9e2263425c7e655fd2d4c
Joseph Daniel Chapdelaine <orvist@gmail.com>
2017-08-23 11:11:30 -0500

    created a button for each match returned on choose job that displays job info

--- client/app/controllers/ChooseJob.js
+++ client/app/controllers/ChooseJob.js
@@ -1,9 +1,10 @@
 "use strict"
 
-  app.controller('ChooseJob', function($scope, $rootScope, MatchService, JobFactory) {
+app.controller('ChooseJob', function($scope, $rootScope, MatchService, JobFactory) {
 
-    $rootScope.lastURL = 'jobs'
-    $scope.getJob = jobNumber => JobFactory.goToJobPage(jobNumber)
-    
-    $scope.Matches = MatchService.getMatches()
-  })
+  $rootScope.lastURL = 'jobs'
+  $scope.Matches = MatchService.getMatches()
+  
+  $scope.goToJob = jobNumber => JobFactory.goToJobPage(jobNumber)
+
+})
