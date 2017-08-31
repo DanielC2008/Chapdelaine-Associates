a5fd69a7d1d2d128131090604d9c461d9ca1f9f0
Joseph Daniel Chapdelaine <orvist@gmail.com>
2017-08-31 10:42:00 -0500

    updated the rest of form factory to match new field/detial format

--- client/app/factories/FormFactory.js
+++ client/app/factories/FormFactory.js
@@ -121,81 +121,352 @@ app.factory('FormFactory', function($mdDialog, CompanyFactory, PropertyFactory) 
 
   factory.getPropertyForm = prop => {
     return {
-      'Primary Address':prop ? prop.primary_address : '', //required
-      'Primary Road':   prop ? prop.primary_road : '', //required if no address
-      'City':           prop ? prop.city : '', 
-      'State':          prop ? prop.state : '', 
-      'Zip Code':       prop ? prop.zip_code : '', 
-      'County':         prop ? prop.county : '',
-      'Property Map':   prop ? prop.property_map : '',
-      'Parcel Number':  prop ? prop.parcel_number : '',
-      'Plat Book':      prop ? prop.plat_book : '',
-      'Plat Page':      prop ? prop.plat_page : '',
-      'Deed Book':      prop ? prop.deed_book : '',  
-      'Deed Page':      prop ? prop.deed_page : '',
-      'Sub Division':   prop ? prop.sub_division : '',
-      'Lot Number':     prop ? prop.lot_number : '',
-      'Acres':          prop ? prop.acres : 0,
-      'Notes':          prop ? prop.notes : ''
+      'Primary Address': {
+        type: 'text',
+        column: 'primary_address',
+        searchable: false,
+        value: prop ? prop.primary_address : '',
+        required: false
+      },
+      'Primary Road': {
+        type: 'text',
+        column: 'primary_road',
+        searchable: false,
+        value: prop ? prop.primary_road : '', //required if no address
+        required: false
+      },
+      'City': {
+        type: 'text',
+        column: 'city',
+        searchable: true,
+        searchFunction: PropertyFactory.searchForCities,
+        value: prop ? prop.city : '',
+        required: false
+      },
+      'State': {
+        type: 'text',
+        column: 'state',
+        searchable: true,
+        searchFunction: PropertyFactory.searchForStates,
+        value: prop ? prop.state : '',
+        required: false
+      },
+      'Zip Code': {
+        type: 'text',
+        column: 'zip_code',
+        searchable: true,
+        searchFunction: PropertyFactory.searchForZipCodes,
+        value: prop ? prop.zip_code : '',
+        required: false
+      },
+      'County': {
+        type: 'text',
+        column: 'county',
+        searchable: true,
+        searchFunction: PropertyFactory.searchForCounties,
+        value: prop ? prop.county : '',
+        required: false
+      },
+      'Map': {
+        type: 'text',
+        column: 'property_map',
+        searchable: false,
+        value: prop ? prop.property_map : '',
+        required: false
+      },
+      'Parcel Number': {
+        type: 'text',
+        column: 'parcel_number',
+        searchable: false,
+        value: prop ? prop.parcel_number : '',
+        required: false
+      },
+      'Plat Book': {
+        type: 'text',
+        column: 'plat_book',
+        searchable: false,
+        value: prop ? prop.plat_book : '',
+        required: false
+      },
+      'Plat Page': {
+        type: 'text',
+        column: 'plat_page',
+        searchable: false,
+        value: prop ? prop.plat_page : '',
+        required: false
+      },
+      'Deed Book': {
+        type: 'text',
+        column: 'deed_book',
+        searchable: false,
+        value: prop ? prop.deed_book : '',  
+        required: false
+      },
+      'Deed Page': {
+        type: 'text',
+        column: 'deed_page',
+        searchable: false,
+        value: prop ? prop.deed_page : '',
+        required: false
+      },
+      'Sub Division': {
+        type: 'text',
+        column: 'sub_division',
+        searchable: false,
+        value: prop ? prop.sub_division : '',
+        required: false
+      },
+      'Lot Number': {
+        type: 'text',
+        column: 'lot_number',
+        searchable: false,
+        value: prop ? prop.lot_number : '',
+        required: false
+      },
+      'Acres': {
+        type: 'number',
+        column: 'acres',
+        searchable: false,
+        value: prop ? prop.acres : 0,
+        required: false
+      },
+      'Notes': {
+        type: 'text',
+        column: 'notes',
+        searchable: false,
+        value: prop ? prop.notes : '',
+        required: false
+      }
     }
   }
 
   factory.getEmployeeForm = employee => {
     return {
-      'First Name':     employee ? employee.first_name : '', //required
-      'Middle Name':    employee ? employee.middle_name : '', 
-      'Last Name':      employee ? employee.last_name : '', //required
-      'S S Number':     employee ? employee.s_s_number : '', 
-      'Date of Birth':  employee ? employee.date_of_birth : null, //date
-      'Marital Status': employee ? employee.marital_status : null, //boolean
-      'U S Citizen':    employee ? employee.u_s_citizen : null, //boolean
-      'Home Phone':     employee ? employee.home_number : '', 
-      'Mobile Phone':   employee ? employee.mobile_number : '',
-      'Start Date':     employee ? employee.start_date : null, //date
-      'End Date':       employee ? employee.end_date : null, //date
-      'Position':       employee ? employee.position : '', 
-      'Pay Rate':       employee ? employee.pay_rate : null, //number
-      'Address':        employee ? employee.address : '', 
-      'City':           employee ? employee.city : '', 
-      'State':          employee ? employee.state : '', 
-      'Zip Code':       employee ? employee.zip_code : '', 
-      'County':         employee ? employee.county : ''
+      'First Name': {
+        type: 'text',
+        column: 'first_name',
+        searchable: false,        
+        value: employee ? employee.first_name : '',
+        required: true
+      },
+      'Middle Name': {
+        type: 'text',
+        column: 'middle_name',
+        searchable: false,        
+        value: employee ? employee.middle_name : '', 
+        required: false
+      },
+      'Last Name': {
+        type: 'text',
+        column: 'last_name',
+        searchable: false,        
+        value: employee ? employee.last_name : '',
+        required: true
+      },
+      'Social Security Number': {
+        type: 'text',
+        column: 's_s_number',
+        searchable: false,        
+        value: employee ? employee.s_s_number : '', 
+        required: false
+      },
+      'Date of Birth': {
+        type: 'date',
+        column: 'date_of_birth',
+        searchable: false,        
+        value: employee ? employee.date_of_birth : null,
+        required: false
+      },
+      'Marital Status': {
+        type: 'checkbox',
+        column: 'marital_status',
+        searchable: false,        
+        value: employee ? employee.marital_status : false,
+        required: false
+      },
+      'U.S. Citizen': {
+        type: 'checkbox',
+        column: 'u_s_citizen',
+        searchable: false,        
+        value: employee ? employee.u_s_citizen : false,
+        required: false
+      },
+      'Home Phone': {
+        type: 'text',
+        column: 'home_phone',
+        searchable: false,        
+        value: employee ? employee.home_phone : '', 
+        required: false
+      },
+      'Mobile Phone': {
+        type: 'text',
+        column: 'mobile_phone',
+        searchable: false,        
+        value: employee ? employee.mobile_phone : '',
+        required: false
+      },
+      'Address': {
+        type: 'text',
+        column: 'address',
+        searchable: true,
+        searchFunction: PropertyFactory.searchForAddresses,
+        value: employee ? employee.address : '',
+        required: false
+      },
+      'City': {
+        type: 'text',
+        column: 'city',
+        searchable: true,
+        searchFunction: PropertyFactory.searchForCities,
+        value: employee ? employee.city : '',
+        required: false
+      },
+      'State': {
+        type: 'text',
+        column: 'state',
+        searchable: true,
+        searchFunction: PropertyFactory.searchForStates,
+        value: employee ? employee.state : '',
+        required: false
+      },
+      'Zip Code': {
+        type: 'text',
+        column: 'zip_code',
+        searchable: true,
+        searchFunction: PropertyFactory.searchForZipCodes,
+        value: employee ? employee.zip_code : '',
+        required: false
+      },
+      'County': {
+        type: 'text',
+        column: 'county',
+        searchable: true,
+        searchFunction: PropertyFactory.searchForCounties,
+        value: employee ? employee.county : '',
+        required: false
+      },
+      'Start Date': {
+        type: 'date',
+        column: 'start_date',
+        searchable: false,        
+        value: employee ? employee.start_date : null,
+        required: false
+      },
+      'End Date': {
+        type: 'date',
+        column: 'end_date',
+        searchable: false,        
+        value: employee ? employee.end_date : null,
+        required: false
+      },
+      'Position': {
+        type: 'text',
+        column: 'position',
+        searchable: false,        
+        value: employee ? employee.position : '', 
+        required: false
+      },
+      'Pay Rate': {
+        type: 'number',
+        column: 'pay_rate',
+        searchable: false,        
+        value: employee ? employee.pay_rate : null,
+        required: false
+      }
     }
   }
 
   factory.getCompanyForm = company => {
     return {
-      'Company Name':   company ? company.company_name : '', //required
-      'Company Address':company ? company.company_address : ''
+      'Company Name': {
+        type: 'text',
+        column: 'company_name',
+        searchable: false,
+        value: company ? company.company_name : '',
+        required: true
+      },
+      'Company Address': {
+        type: 'text',
+        column: 'company_address',
+        searchable: false,
+        value: company ? company.company_address : '',
+        required: false
+      }
     }
   }
 
   factory.getTaskForm = () => {
     return {
-      'Task': '',
-      'Rate': null, //number
-      'Hourly': null //boolean
+      'Task': {
+        type: 'text',
+        column: 'task',
+        searchable: false,        
+        value: '', 
+        required: true
+      },
+      'Rate': {
+        type: 'number',
+        column: 'rate',
+        searchable: false,        
+        value: null, 
+        required: true
+      },
+      'Hourly': {
+        type: 'checkbox',
+        column: 'hourly',
+        searchable: false,        
+        value: false, 
+        required: false
+      }
     }
   }
 
   factory.getAddressForm = () => {
     return {
-      'Address': ''
+      'Address': {
+        type: 'text',
+        column: 'address',
+        searchable: false,        
+        value: '', 
+        required: true
+      }
     }
   }
 
   factory.getRoadForm = () => {
     return {
-      'Road': ''
+      'Road': {
+        type: 'text',
+        column: 'road',
+        searchable: false,        
+        value: '', 
+        required: true
+      }
     }
   }
 
   factory.getJobTypeForm = () => {
-    return {'Job Type': ''}
+    return {
+      'Job Type': {
+        type: 'text',
+        column: 'job_type',
+        searchable: false,        
+        value: '', 
+        required: true
+      }
+    }
   }
 
   factory.getCauseForm = () => {
-    return {'Cause': ''}
+    return {
+      'Cause': {
+        type: 'text',
+        column: 'cause',
+        searchable: false,        
+        value: '', 
+        required: true
+      }
+    }
   }
 
   factory.updateForm = (table, existingObj, ids, formType) => {
