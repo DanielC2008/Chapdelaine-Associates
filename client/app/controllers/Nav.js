52191d73e573d57afe3e2bd49d468784b1e82c11
Joseph Daniel Chapdelaine <orvist@gmail.com>
2017-09-19 11:36:35 -0500

    when job number is searched, checks to see if job number exists before going to job

--- client/app/controllers/Nav.js
+++ client/app/controllers/Nav.js
@@ -1,11 +1,17 @@
 'use strict'
 
-app.controller('Nav', function($scope, JobFactory, $mdDialog) {
+app.controller('Nav', function($scope, JobFactory, $mdDialog, AlertFactory) {
   let NAV = this
 
   NAV.submit = jobNumber => {
-    JobFactory.goToJobPage(jobNumber)
-    NAV.jobNumber = ''
+    JobFactory.checkJobNumberExists(jobNumber).then( ({ data: { exists } }) => {
+      if (exists) {
+        JobFactory.goToJobPage(jobNumber)
+        NAV.jobNumber = ''        
+      } else {
+        AlertFactory.toastReject(`Job Number ${jobNumber} doesn't exist!`)
+      }
+    }).catch( err => console.log('err', err))
   }
 
   NAV.newJob = () =>{
